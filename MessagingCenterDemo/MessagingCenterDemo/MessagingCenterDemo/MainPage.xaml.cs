8aa940cb4d46989f2b89953d73b5b1e4c9fbb58a
Enthusiastic_coder <21123907+shubhambaghel34@users.noreply.github.com>
2018-01-23 12:45:17 +0530

    Message center demo
    App to demonstrate message center app

---
+++ MessagingCenterDemo/MessagingCenterDemo/MessagingCenterDemo/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MessagingCenterDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        Entry entry1;
+        public MainPage()
+        {
+            BindingContext = new MainPageViewModel();
+
+            // Send messages when buttons are pressed
+            var button1 = new Button
+            {
+                Text = "Say Hi",
+                BackgroundColor = Color.CadetBlue
+            };
+            button1.Clicked += (sender, e) =>
+            {
+                 MessagingCenter.Send<MainPage,string>(this, "Hi", entry1.Text);
+               
+            };
+            var button2 = new Button
+            {
+                Text = "Say Hi to Json",
+                BackgroundColor = Color.DarkCyan
+            };
+            button2.Clicked += (sender, e) =>
+            {
+                MessagingCenter.Send<MainPage, string>(this, "Hi", "John");
+            };
+
+            var button3 = new Button
+            {
+                Text = "Unsubscribe from alert",
+                BackgroundColor = Color.DarkGreen
+            };
+             entry1 = new Entry
+            {
+                Placeholder = "Enter the text",
+                BackgroundColor = Color.Firebrick
+
+            };
+            button3.Clicked += (sender, e) =>
+            {
+                MessagingCenter.Unsubscribe<MainPage, string>(this, "Hi");
+                DisplayAlert("Unsubscribed",
+                    "This page has stopped listening, so no more alerts; however the ViewModel is still receiving messages.",
+                    "OK");
+            };
+
+            // Subscribe to a message (which the ViewModel has also subscribed to) to pop up an Alert
+            MessagingCenter.Subscribe<MainPage, string>(this, "Hi", (sender, arg) =>
+            {
+                DisplayAlert("Message Received", "arg=" + arg, "OK");
+            });
+
+            var listView = new ListView();
+            listView.SetBinding(ListView.ItemsSourceProperty, "Greetings");
+
+            Content = new StackLayout
+            {
+                Padding = new Thickness(0, 20, 0, 0),
+                Children = {  button1, entry1, button2, button3, listView }
+            };
+        }
+    }
+}
---
+++ MessagingCenterDemo/MessagingCenterDemo/MessagingCenterDemo/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace MessagingCenterDemo
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<string> Greetings { get; set; }
+
+        public MainPageViewModel()
+        {
+            Greetings = new ObservableCollection<string>();
+
+            MessagingCenter.Subscribe<MainPage>(typeof(MainPage), "Hi", (sender) => 
+            {
+                Greetings.Add("Hi");
+            });
+
+            MessagingCenter.Subscribe<MainPage, string>(typeof(MainPage), "Hi", (sender, arg) => 
+            {
+                Greetings.Add("Hi " + arg);
+            });
+        }
+    }
+}
